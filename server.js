a9c79df36b454e7468745ccd9af4bf3444e904ad
claire stranack <claire@stranack.co.uk>
2022-02-06 15:57:12 +0000

    first Commit

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
+var handlebars = require('express-handlebars');
+
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+    layoutDir: __dirname + '/views/layouts',
+    extname: 'hbs'
+}));
 
 //web application getting something from the server
 //req = request //res = response //=>'fat arrow' = function
+//presenting the index page with the main layout
 app.get('/', (req, res) => {
-    res.send('This is the homepage :D ');
+    res.render('index', {layout: 'main'}); 
 
 });
 //second page
