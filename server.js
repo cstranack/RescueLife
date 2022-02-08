bac099cae048587b2650027723dd8afdc031e8d7
claire stranack <claire@stranack.co.uk>
2022-02-08 21:36:24 +0000

    server working

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+// requiring all necesssary packages
 var express = require('express');
+// const { engine } = require('express-handlebars');
 var app = express();
 var handlebars = require('express-handlebars');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
 
 app.set('view engine', 'hbs');
-app.engine('hbs', handlebars({
-    layoutDir: __dirname + '/views/layouts',
-    extname: 'hbs'
+app.engine('hbs', handlebars.engine({
+     layoutsDir: __dirname + '/views/layouts',
+     extname: 'hbs'
 }));
 
 //web application getting something from the server
@@ -13,19 +18,26 @@ app.engine('hbs', handlebars({
 //presenting the index page with the main layout
 app.get('/', (req, res) => {
     res.render('index', {layout: 'main'}); 
-
 });
+
 //second page
-app.get('/about', (req, res) => {
-    res.send('This is the about page :D ');
+app.get('/profile', (req, res) => {
+    res.send('This is the profile page :D ');
 
 });
 
-//third page
-app.get('/contact', (req, res) => {
-    res.send('This is the contact page :D ');
 
+mongoose.connect('mongodb://localhost:27017/handlebars', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+.then(() => {
+    console.log('connected to the DB ');
+})
+.catch((err) => {
+    console.log('Not connected to DB with err :' + err);
 });
+
 
 
 //listening for requests on port 3000
