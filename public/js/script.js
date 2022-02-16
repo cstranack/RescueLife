09ff68800be34d17cc44d93a1093e39e2983a712
claire stranack <claire@stranack.co.uk>
2022-02-16 15:23:06 +0000

    authetiation working

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,15 @@
 const usernameField = document.querySelector("#username");
 const signUpSubmit = document.querySelector("#signUpSubmit");
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirmPassword');
 
 signUpSubmit.addEventListener('click', (e) => {
     if (usernameField.value === '') {
         e.preventDefault();
-        window.alert('Form Requires Username')
+        window.alert('Form Requires Username');
     }
-})
+    if(password.value != confirmPassword.value){
+        e.preventDefault();
+        window.alert('Passwords do not match');
+    }
+});
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 // requiring all necesssary packages
 var express = require('express');
-// const { engine } = require('express-handlebars');
 var app = express();
 var handlebars = require('express-handlebars');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var Contact = require('./models/Contact');
 var User = require('./models/User');
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var { isAuth } = require('./middleware/isAuth');
+var session = require('express-session');
+require('./middleware/passport')(passport);
 
-
+//establishing handlbars structure
 app.set('view engine', 'hbs');
 app.engine('hbs', handlebars.engine({
      layoutsDir: __dirname + '/views/layouts',
@@ -16,21 +20,30 @@ app.engine('hbs', handlebars.engine({
 }));
 
 app.use(express.static('public'));
+app.use(session({
+    secret: 'mySecret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 
 //presenting the index page with the main layout
-app.get('/dashboard', (req, res) => {
+app.get('/dashboard', isAuth, (req, res) => {
     Contact.find({}).lean()
     .exec((err, contacts) =>{
         if(contacts.length){
-            res.render('dashboard', {layout: 'main', contacts: contacts, contactsExist: true}); 
+            res.render('dashboard', {layout: 'main', contacts: contacts, contactsExist: true, username: req.user.username }); 
         } else{
             res.render('dashboard', {layout: 'main', contacts: contacts, contactsExist: false}); 
         }
-    })
+    });
     //to find a specific thing
     //Contact.find({name: 'Claire'}).lean()
 });
@@ -41,13 +54,44 @@ app.get('/', (req, res) => {
 });
 
 //sending data to the database
-app.post('/signup', (req, res) =>{
+app.post('/signup', async (req, res) =>{
     const { username, password } = req.body;
-    var user = new User({
-        username,
-        password
-    });
-    user.save();
+    try{
+        let user = await User.findOne({ username });
+        if (user) {
+            return res.status(400).render('login', {layout: 'main', userExist: true});
+        }
+        user = new User({
+            username,
+            password
+        });
+        //password encryption 
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+        res.status(200).render('login', {layout: 'main', userDoesNotExist: true});
+    } catch (err){
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+app.post('/signin', (req, res, next) => {
+    try{
+        passport.authenticate('local',{
+            successRedirect: '/dashboard',
+            failureRedirect: '/'
+        })(req, res, next);
+    } catch(err){
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+app.get('/signout', (req, res) =>{
+    req.logout();
     res.redirect('/');
 });
 
