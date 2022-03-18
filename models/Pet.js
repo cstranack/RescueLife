81548409f6eb34939f2637f1fe2cede0b9ed77b9
claire stranack <claire@stranack.co.uk>
2022-03-18 16:56:33 +0000

    updated pet schema typo

--- models/Pet.js
+++ models/Pet.js
@@ -1,35 +1,35 @@
 var mongoose = require('mongoose');
 
 const PetSchema = new mongoose.Schema({
-    user:{
+    pet:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'users'
+        ref: 'pets'
     },
     petName: {
         type: String
     },
-    adoptable: {
-        type: Boolean
-    },
-    category: {
-        type: String, possibleValues: ['Dog','Cat','Equine','Other']
-    },
-    breed: {
-        type: String
-    },
-    species: {
-        type: String
-    },
-    age: {
-        type: Number
-    },
-    size: {
-        type: String
-    },
-    hypo: {
-        type: Boolean,
-        // default: False
-    },
+    // adoptable: {
+    //     type: Boolean
+    // },
+    // category: {
+    //     type: String, possibleValues: ['Dog','Cat','Equine','Other']
+    // },
+    // breed: {
+    //     type: String
+    // },
+    // species: {
+    //     type: String
+    // },
+    // age: {
+    //     type: Number
+    // },
+    // size: {
+    //     type: String
+    // },
+    // hypo: {
+    //     type: Boolean,
+    //     // default: False
+    // },
     date: {
         type: Date,
         default: Date.now
